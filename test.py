5eaa0c30da3ec0501aa6cf92b8376fdff4867985
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2024-11-25 23:30:31 +0400

    Added confusion matrix

--- test.py
+++ test.py
@@ -1,24 +1,35 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms
 import numpy as np
 
 import matplotlib.pyplot as plt
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
+import seaborn as sns  
 
 from BNext_model import BNext
+
+
+def plot_confusion_matrix(cm, classes):
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(
+        cm, annot=True, fmt="d", cmap="Blues", xticklabels=classes, yticklabels=classes
+    )
+    plt.xlabel("Predicted Labels")
+    plt.ylabel("True Labels")
+    plt.title("Confusion Matrix")
+    plt.show()
 
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    # Initialize and load the model
     model = nn.DataParallel(BNext(num_classes=2)).to(device)
-
     model_path = "best_model.pth"
     model.load_state_dict(torch.load(model_path, weights_only=False))
+    model.eval()
 
     # Preprocessing (Normalization)
     transform = transforms.Compose(
@@ -39,14 +50,11 @@ def main():
 
     test_loader = DataLoader(test_subset, batch_size=512, shuffle=False, num_workers=4)
 
-    # Set model to evaluation mode
-    model.eval()
-
     # Initialize variables to track correct predictions and total number of samples
     correct = 0
     total = 0
 
-    # Lists to store true labels and predictions for precision, recall, and F1-score
+    # Lists to store true labels and predictions
     all_labels = []
     all_preds = []
 
@@ -55,7 +63,6 @@ def main():
             inputs, labels = inputs.to(device), labels.to(device)
 
             outputs = model(inputs)
-
             _, predicted = torch.max(outputs, 1)
 
             total += labels.size(0)
@@ -75,6 +82,11 @@ def main():
     print(f"Precision: {precision:.2f}")
     print(f"Recall: {recall:.2f}")
     print(f"F1-Score: {f1:.2f}")
+
+    # Calculate and plot confusion matrix
+    cm = confusion_matrix(all_labels, all_preds)
+    class_names = test_dataset.classes  # Get class names from the dataset
+    plot_confusion_matrix(cm, classes=class_names)
 
 
 if __name__ == "__main__":
